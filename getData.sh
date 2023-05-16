9cb57650f385ad9fcd98871a98bee5accdbd26ce
ausrinezele <ausrine.zele@gmail.com>
2023-05-16 11:56:23 +0300

    test

--- getData.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Download raw FASTQ files
-prefetch ERR204044
-prefetch SRR15131330 SRR18214264
-fastq-dump --gzip --split-files ERR204044 SRR15131330 SRR18214264
-mv code/* input/ && mv input/*.sh code/
